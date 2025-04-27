475c8d1678135a1ce9a66262f21df5d03a273531
marcelo-bn <marcelo.filho@medway.com.br>
2025-04-26 22:09:08 -0300

    user name

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,6 +9,7 @@ export default async function DashboardLayout({
 }) {
 
   const session = await auth();
+  const userName = session?.user?.name;
 
   if (!session) {
     redirect('/login');
@@ -16,7 +17,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar userName={'Fulano de Tal'} />
+      <Navbar userName={userName ?? ''} />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {children}
       </main>
--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logo from './assets/logo.svg';
 import womanImg from './assets/woman.svg';
+import { auth } from '@/auth';
 
 export default async function Home() {
+  const session = await auth();
+  const userName = session?.user?.name ?? '';
+
   return (
     <main>
       <section className="container mx-auto text-center pb-20 px-2 md:px-0">
@@ -46,9 +50,16 @@ export default async function Home() {
             <Link href={'#preco'}>
               <Button variant={'link'}>Preço</Button>
             </Link>
-            <Link href="/login">
-              <Button variant={'bg-white'}>Login</Button>
-            </Link>
+            {session && (
+              <Link href="/dashboard">
+                <Button variant={'bg-white'}>Dashboard</Button>
+              </Link>
+            )}
+            {!session && (
+              <Link href="/login">
+                <Button variant={'bg-white'}>Login</Button>
+              </Link>
+            )}
           </div>
         </nav>
         <h1 className="md:text-6xl text-2xl font-bold mt-8 md:mt-16">
